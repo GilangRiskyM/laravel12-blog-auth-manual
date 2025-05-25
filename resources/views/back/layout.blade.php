e3c0005ba77981bae0c5000668b37910dafd8618
Gilang Risky M <gilangriskymahardhika@gmail.com>
2025-05-25 21:20:33 +0700

    ganti bootstrap ke lokal

--- resources/views/back/layout.blade.php
+++ resources/views/back/layout.blade.php
@@ -11,13 +11,10 @@
     <link rel="icon" href="{{ asset('img/icon1.png') }}" type="image/x-icon">
 
     {{-- bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-        </script>
-    <link href="/back/css/styles.css" rel="stylesheet" />
-    <script src="/back/js/scripts.js"></script>
+    <link rel="stylesheet" href="{{ url('/bootstrap-5.2.3-dist/css/bootstrap.min.css') }}">
+    <script src="{{ url('/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js') }}"></script>
+    <link href="{{ url('/back/css/styles.css') }}" rel="stylesheet" />
+    <script src="{{ url('/back/js/scripts.js') }}"></script>
 
     {{-- boxicons --}}
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -25,15 +22,15 @@
     {{-- summernote-lite --}}
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-        </script>
+    </script>
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-lite.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-lite.min.js"></script>
 
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="/dashboard"><img src="{{ asset('img/icon1.png') }}" alt="Logo" width="40"
-                height="40">
+        <a class="navbar-brand ps-3" href="/dashboard"><img src="{{ asset('img/icon1.png') }}" alt="Logo"
+                width="40" height="40">
             {{ Auth::user()->name }}
         </a>
         <!-- Sidebar Toggle-->
--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -17,8 +17,7 @@
         href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
         rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('/bootstrap-5.2.3-dist/css/bootstrap.min.css') }}">
     <link href="{{ url('/front/css/styles.css') }}" rel="stylesheet" />
     @stack('css')
 </head>
@@ -135,7 +134,7 @@
         </div>
     </footer>
     <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js') }}"></script>
     <!-- Core theme JS-->
     <script src="{{ url('/front/js/scripts.js') }}"></script>
 </body>
